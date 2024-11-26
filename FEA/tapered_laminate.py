c5ab02f6162d54f7061780e32ad5bdd1a1c975ac
prateekr9SF <119811887+prateekr9SF@users.noreply.github.com>
2024-11-26 14:18:33 -0500

    enable taper

--- FEA/tapered_laminate.py
+++ FEA/tapered_laminate.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.tri as tri
-
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # Plate geometry
 Lx = 1.0  # Chord length (m)
@@ -11,14 +10,9 @@ ny = 20    # Number of elements along the span (y-direction)
 
 
 def generateMesh(Lx, Ly, nx, ny):
-    # Uniformly spaced x and y coordinates
     x_coords = np.linspace(0, Lx, nx + 1)
     y_coords = np.linspace(0, Ly, ny + 1)
-    
-    # Generate nodes
     nodes = np.array([[x, y] for y in y_coords for x in x_coords])
-    
-    # Define element connectivity
     elements = []
     for j in range(ny):
         for i in range(nx):
@@ -28,98 +22,42 @@ def generateMesh(Lx, Ly, nx, ny):
             n4 = n1 + (nx + 1)
             elements.append([n1, n2, n3, n4])
     elements = np.array(elements)
-    
     return x_coords, y_coords, nodes, elements
-    #end
-    
+
+
 x_coords, y_coords, nodes, elements = generateMesh(Lx, Ly, nx, ny)
 
 
 # Degrees of freedom per node
 dof_per_node = 3  # w, theta_x, theta_y (default for RM theory)
 
-def define_symmetric_laminate(base_ply, stacking_sequence):
-    """
-    Define a symmetric laminate based on a stacking sequence.
+# Define the tapering function
+def compute_tapered_thickness(x, y, Lx, Ly, t_min, t_max):
+    return t_min + (t_max - t_min) * (Ly - y) / Ly
 
-    Parameters:
-        base_ply : dict
-            Material properties of a single ply.
-        stacking_sequence : list
-            List of ply angles (e.g., [0, 45, -45, 90]).
-    
-    Returns:
-        list : Symmetric laminate definition as a list of ply dictionaries.
-    """
+
+def define_symmetric_laminate(base_ply, stacking_sequence):
     laminate = []
-    
-    # Create plies for the given stacking sequence
     for angle in stacking_sequence:
         ply = base_ply.copy()
         ply["angle"] = angle
         laminate.append(ply)
-    
-    # Add symmetric plies by mirroring the sequence
-    symmetric_part = laminate[::-1]  # Reverse order for mirroring
+    symmetric_part = laminate[::-1]
     laminate += symmetric_part
-    
     return laminate
 
-# Define base ply material properties
+
 base_ply = {
-    "E1": 140e9,    # Longitudinal modulus (Pa)
-    "E2": 10e9,     # Transverse modulus (Pa)
-    "G12": 5e9,     # Shear modulus (Pa)
-    "nu12": 0.3,    # Poisson's ratio
-    "thickness": 0.000125,  # Thickness (m)
-    "density": 1600  # Density (kg/m^3)
+    "E1": 140e9,
+    "E2": 10e9,
+    "G12": 5e9,
+    "nu12": 0.3,
+    "thickness": 0.000125,
+    "density": 1600
 }
-
-# Define the stacking sequence (half laminate)
 stacking_sequence = [0, 45, -45, 90]
-
-# Generate the symmetric laminate
 plies = define_symmetric_laminate(base_ply, stacking_sequence)
 
-
-
-
-# Select theory
-use_reissner_mindlin = True  # Set to False to use Classical Laminate Theory
-
-
-# Helper functions
-def compute_ply_stiffness(ply):
-    """Compute the transformed stiffness matrix Q* for a ply."""
-    E1, E2, G12, nu12 = ply["E1"], ply["E2"], ply["G12"], ply["nu12"]
-    angle = np.radians(ply["angle"])
-    Q = np.array([
-        [E1 / (1 - nu12 * E2 / E1), nu12 * E2 / (1 - nu12 * E2 / E1), 0],
-        [nu12 * E2 / (1 - nu12 * E2 / E1), E2 / (1 - nu12 * E2 / E1), 0],
-        [0, 0, G12]
-    ])
-    c, s = np.cos(angle), np.sin(angle)
-    T = np.array([
-        [c**2, s**2, 2 * c * s],
-        [s**2, c**2, -2 * c * s],
-        [-c * s, c * s, c**2 - s**2]
-    ])
-    return T @ Q @ T.T
-
-def compute_abd_matrix(plies):
-    """Compute the A, B, D stiffness matrices for the laminate."""
-    A = np.zeros((3, 3))
-    B = np.zeros((3, 3))
-    D = np.zeros((3, 3))
-    z_prev = -sum(ply["thickness"] for ply in plies) / 2
-    for ply in plies:
-        z_next = z_prev + ply["thickness"]
-        Q_star = compute_ply_stiffness(ply)
-        A += Q_star * (z_next - z_prev)
-        B += 0.5 * Q_star * (z_next**2 - z_prev**2)
-        D += (1 / 3) * Q_star * (z_next**3 - z_prev**3)
-        z_prev = z_next
-    return A, B, D
 
 def apply_elliptic_pressure(nodes, dof_per_node, load_vector, pressure_max, a, b):
     """
@@ -190,14 +128,46 @@ def apply_ply_weight(nodes, dof_per_node, load_vector, plies, gravity=9.81):
     
     return load_vector
 
+def compute_ply_stiffness(ply):
+    E1, E2, G12, nu12 = ply["E1"], ply["E2"], ply["G12"], ply["nu12"]
+    angle = np.radians(ply["angle"])
+    Q = np.array([
+        [E1 / (1 - nu12 * E2 / E1), nu12 * E2 / (1 - nu12 * E2 / E1), 0],
+        [nu12 * E2 / (1 - nu12 * E2 / E1), E2 / (1 - nu12 * E2 / E1), 0],
+        [0, 0, G12]
+    ])
+    c, s = np.cos(angle), np.sin(angle)
+    T = np.array([
+        [c**2, s**2, 2 * c * s],
+        [s**2, c**2, -2 * c * s],
+        [-c * s, c * s, c**2 - s**2]
+    ])
+    return T @ Q @ T.T
 
-def compute_element_stiffness(xy, D_b, t, kappa, G13, G23, use_reissner_mindlin):
-    """Compute the element stiffness matrix."""
+
+def compute_abd_matrix_tapered(plies, nodes, elements, thickness_func):
+    A = np.zeros((3, 3))
+    B = np.zeros((3, 3))
+    D = np.zeros((3, 3))
+    for elem in elements:
+        xy = nodes[elem, :]
+        avg_thickness = np.mean([thickness_func(x, y, Lx, Ly, t_min, t_max) for x, y in xy])
+        z_prev = -sum(ply["thickness"] for ply in plies) / 2 * avg_thickness
+        for ply in plies:
+            z_next = z_prev + ply["thickness"] * avg_thickness
+            Q_star = compute_ply_stiffness(ply)
+            A += Q_star * (z_next - z_prev)
+            B += 0.5 * Q_star * (z_next**2 - z_prev**2)
+            D += (1 / 3) * Q_star * (z_next**3 - z_prev**3)
+            z_prev = z_next
+    return A, B, D
+
+
+def compute_element_stiffness_tapered(xy, thickness_func, D_b, kappa, G13, G23, use_reissner_mindlin):
     gauss_points = [(-np.sqrt(1/3), -np.sqrt(1/3)), (np.sqrt(1/3), -np.sqrt(1/3)),
                     (np.sqrt(1/3), np.sqrt(1/3)), (-np.sqrt(1/3), np.sqrt(1/3))]
     weights = [1, 1, 1, 1]
     ke = np.zeros((12, 12))
-
     for gp, w in zip(gauss_points, weights):
         xi, eta = gp
         dN_dxi = np.array([
@@ -208,39 +178,37 @@ def compute_element_stiffness(xy, D_b, t, kappa, G13, G23, use_reissner_mindlin)
         det_J = np.linalg.det(J)
         inv_J = np.linalg.inv(J)
         dN_dx = np.dot(inv_J, dN_dxi)
-
+        avg_thickness = np.mean([thickness_func(x, y, Lx, Ly, t_min, t_max) for x, y in xy])
         B_b = np.zeros((3, 12))
         for i in range(4):
             B_b[0, i * 3 + 1] = dN_dx[0, i]
             B_b[1, i * 3 + 2] = dN_dx[1, i]
             B_b[2, i * 3 + 1] = dN_dx[1, i]
             B_b[2, i * 3 + 2] = dN_dx[0, i]
-
-        ke += det_J * w * (np.dot(B_b.T, np.dot(D_b, B_b)))
-
+        ke += det_J * w * (np.dot(B_b.T, np.dot(D_b, B_b)) * avg_thickness)
         if use_reissner_mindlin:
             B_s = np.zeros((2, 12))
             for i in range(4):
                 B_s[0, i * 3] = dN_dx[0, i]
                 B_s[1, i * 3] = dN_dx[1, i]
-            D_s = kappa * t * np.diag([G13, G23])
+            D_s = kappa * avg_thickness * np.diag([G13, G23])
             ke += det_J * w * (np.dot(B_s.T, np.dot(D_s, B_s)))
-
     return ke
 
-# Compute ABD matrix
-A, B, D = compute_abd_matrix(plies)
 
-# Global stiffness matrix
+t_min = 0.0001
+t_max = 0.001
+
+A, B, D = compute_abd_matrix_tapered(plies, nodes, elements, compute_tapered_thickness)
+
 num_nodes = nodes.shape[0]
 num_dof = num_nodes * dof_per_node
 K = np.zeros((num_dof, num_dof))
-
 for elem in elements:
     xy = nodes[elem, :]
-    ke = compute_element_stiffness(xy, D, t=sum(ply["thickness"] for ply in plies),
-                                   kappa=5/6, G13=5e9, G23=5e9,
-                                   use_reissner_mindlin=use_reissner_mindlin)
+    ke = compute_element_stiffness_tapered(
+        xy, compute_tapered_thickness, D, kappa=5/6, G13=5e9, G23=5e9, use_reissner_mindlin=True
+    )
     element_dofs = []
     for node in elem:
         element_dofs.extend([node * dof_per_node, node * dof_per_node + 1, node * dof_per_node + 2])
